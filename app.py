713fe32ade21fbef5e095f2940e9746847410c53
andrzej76228 <101048874+andrzej76228@users.noreply.github.com>
2022-04-04 14:46:01 +0200

    created app.py, Dockerfile and requirements.txt

---
+++ app.py
@@ -0,0 +1,16 @@
+"""List 3"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Function return h1 tag at endpoint 127.0.0.1:5000/
+    """
+    return "<h1>Hello WSB! Greetings from Flask & Docker!<h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
